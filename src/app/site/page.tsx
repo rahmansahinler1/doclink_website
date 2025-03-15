444d488279b3de2c28164ea440edb2b2f7383399
karstaag <rahmansahinler1@gmail.com>
2025-03-15 10:54:50 +0300

    feat: add site layout and home page components for improved structure and navigation

---
+++ src/app/site/page.tsx
@@ -0,0 +1,26 @@
+import ScrollUp from "@/components/Common/ScrollUp";
+import Hero from "@/components/Hero";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth/next";
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: "Doclink",
+  description: "Document question answering application",
+};
+
+export default async function Home() {
+  const session = await getServerSession();
+
+  // If the user is already logged in, redirect to the dashboard
+  if (session && session.user) {
+    redirect('/dashboard');
+  }
+
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+    </>
+  );
+} 
